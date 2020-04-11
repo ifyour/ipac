824df7bc62bb5a6e1a52e1db0e1a4b62198bf49f
ifyour <ifyour@outlook.com>
2020-04-11 17:50:15 +0800

    feat: build template from template.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const path = require('path');
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
 const YAML = require('yamljs');
 const { format, utcToZonedTime } = require('date-fns-tz');
 
-// source + teplate  = dist.js
+/**
+ * index.js
+ * template.js + white_domains.yml  = dist.js (pac file)
+ */
 const DOMAINS_YML = './white_domains.yml';
-const STRING_FILE = './templateString';
+const TEMPLATE = './template.js';
 const DIST_FILE = './dist.js';
 
 const TIME_FORMAT = 'yyyy-MM-dd HH:mm:ss';
 const ZONE_DATE = utcToZonedTime(new Date(), 'Asia/Shanghai');
 const SAVE_CONTENT_ENCODE = 'utf-8';
-
 const NOW_TIME = format(ZONE_DATE, TIME_FORMAT);
-const TEMPLATE_STRING = require(STRING_FILE);
 const resolveFile = file => path.join(__dirname, file);
 const saveFile = (target, content) => writeFileSync(resolveFile(target), content, SAVE_CONTENT_ENCODE);
 
+const buffer = readFileSync(resolveFile(TEMPLATE));
+const templateString = buffer.toString('utf8');
+
 const domains = YAML.load(resolveFile(DOMAINS_YML));
-const distContent = TEMPLATE_STRING
+const distContent = templateString
   .replace(/#TEMPLATE_TIME#/g, NOW_TIME)
   .replace(/'#TEMPLATE_CONTENT#'/g, JSON.stringify(domains, null, 2));
 
