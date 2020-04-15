81451511eb711b1a9b54b9c1d6c7be5c137884fa
Willard Wong <ifyour@outlook.com>
2020-04-15 14:04:23 +0000

    fix: params as object

--- api/port.js
+++ api/port.js
@@ -8,8 +8,8 @@ const { format, utcToZonedTime } = require('date-fns-tz');
 
 function generatePac(
   {
-    socks5Port,
-    httpPort,
+    socks5,
+    http,
     s = 1080,
     h = 1087,
     ip = '127.0.0.1',
@@ -33,8 +33,8 @@ function generatePac(
       const data = buffer.toString('utf8')
         .replace(/#TEMPLATE_TIME#/g, NOW_TIME)
         .replace(/'#TEMPLATE_CONTENT#'/g, JSON.stringify(domains, null, 2))
-        .replace(/:1080;/g, `:${socks5Port || s};`)
-        .replace(/:1087;/g, `:${httpPort || h};`)
+        .replace(/:1080;/g, `:${socks5 || s};`)
+        .replace(/:1087;/g, `:${http || h};`)
         .replace(/127\.0\.0\.1/g, ip);
 
       resolve(data);
@@ -46,8 +46,8 @@ function generatePac(
 
 
 module.exports = async (req, res) => {
-  const { query: { socks5, http, s, h, ip }} = req;
+  const { query }  req;
   res.setHeader('content-type', 'application/javascript; charset=utf-8');
-  const pacContent = await generatePac({ socks5, http, s, h, ip });
+  const pacContent = await generatePac(query);
   res.send(pacContent);
 }
