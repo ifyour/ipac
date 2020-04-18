2492798ef15277b37da776b560c209092ca0a752
ifyour <ifyour@outlook.com>
2020-04-18 14:10:10 +0800

    fix: eslint & optimize

--- api/port.js
+++ api/port.js
@@ -1,53 +1,30 @@
-const { writeFileSync, readFileSync } = require('fs');
+const { readFileSync } = require('fs');
 const path = require('path');
 const yamljs = require('yamljs');
 const axios = require('axios');
 const get = require('lodash.get');
 const { format, utcToZonedTime } = require('date-fns-tz');
 
-
-function generatePac(
-  {
-    socks5,
-    http,
-    s = 1080,
-    h = 1087,
-    ip = '127.0.0.1',
-  }
-) {
+async function generatePac({ socks5, http, s = 1080, h = 1087, ip = '127.0.0.1' }) {
   const TEMPLATE = '../template.js';
   const DOMAINS_YML = '../white_domains.yml';
-  const TIME_FORMAT = 'yyyy-MM-dd HH:mm:ss';
-
-  return new Promise(async (resolve, reject) => {
-    try {
-      const repoDetail = await axios.get('https://api.github.com/repos/ifyour/ipac/branches/master');
-      const lastBuildTime = get(repoDetail, 'data.commit.commit.committer.date') || new Date();
-
-      const ZONE_DATE = utcToZonedTime(lastBuildTime, 'Asia/Shanghai');
-      const NOW_TIME = format(ZONE_DATE, TIME_FORMAT);
-
-      const domains = yamljs.load(path.resolve(__dirname, DOMAINS_YML));
-      const buffer = readFileSync(path.resolve(__dirname, TEMPLATE));
-
-      const data = buffer.toString('utf8')
-        .replace(/#TEMPLATE_TIME#/g, NOW_TIME)
-        .replace(/'#TEMPLATE_CONTENT#'/g, JSON.stringify(domains, null, 2))
-        .replace(/:1080;/g, `:${socks5 || s};`)
-        .replace(/:1087;/g, `:${http || h};`)
-        .replace(/127\.0\.0\.1/g, ip);
-
-      resolve(data);
-    } catch (error) {
-      reject(error);
-    }
-  })
+  const repoDetail = await axios.get('https://api.github.com/repos/ifyour/ipac/branches/master');
+  const lastBuildTime = get(repoDetail, 'data.commit.commit.committer.date') || new Date();
+  const ZONE_DATE = utcToZonedTime(lastBuildTime, 'Asia/Shanghai');
+  const NOW_TIME = format(ZONE_DATE, 'yyyy-MM-dd HH:mm:ss');
+  const domains = yamljs.load(path.resolve(__dirname, DOMAINS_YML));
+  const buffer = readFileSync(path.resolve(__dirname, TEMPLATE));
+  return buffer.toString('utf8')
+    .replace(/#TEMPLATE_TIME#/g, NOW_TIME)
+    .replace(/'#TEMPLATE_CONTENT#'/g, JSON.stringify(domains, null, 2))
+    .replace(/:1080;/g, `:${socks5 || s};`)
+    .replace(/:1087;/g, `:${http || h};`)
+    .replace(/127\.0\.0\.1/g, ip);
 }
-
 
 module.exports = async (req, res) => {
   const { query } = req;
   res.setHeader('content-type', 'application/javascript; charset=utf-8');
   const pacContent = await generatePac(query);
   res.send(pacContent);
-}
+};
--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const TEMPLATE = './template.js';
 const HTTPS_TEMPLATE = './https_tmp.js';
 const DOMAINS_YML = './white_domains.yml';
 
+// extends tmplate
 contentExtends({
   source: TEMPLATE,
   target: HTTPS_TEMPLATE,
   contentFrom: 'SOCKS5 127.0.0.1:1080;',
   contentTo: 'HTTPS haotizi.tk:443;',
-})
+});
 
-// generatePac(TEMPLATE, DOMAINS_YML, './dist.js');
+// build pac (template + domains)
 generatePac(HTTPS_TEMPLATE, DOMAINS_YML, './dist_https.js');
 
 /**
@@ -29,20 +30,14 @@ generatePac(HTTPS_TEMPLATE, DOMAINS_YML, './dist_https.js');
 function generatePac(template, domainsData, dist) {
   const TIME_FORMAT = 'yyyy-MM-dd HH:mm:ss';
   const ZONE_DATE = utcToZonedTime(new Date(), 'Asia/Shanghai');
-  const CONTENT_ENCODE = 'utf-8';
   const NOW_TIME = format(ZONE_DATE, TIME_FORMAT);
-
-  const resolveFile = file => path.join(__dirname, file);
-  const saveFile = (target, content) => writeFileSync(
-    resolveFile(target), content, CONTENT_ENCODE
-  );
-  const buffer = readFileSync(resolveFile(template));
+  const resolve = file => path.join(__dirname, file);
+  const buffer = readFileSync(resolve(template));
   const templateString = buffer.toString('utf8');
-  const domains = yamljs.load(resolveFile(domainsData));
+  const domains = yamljs.load(resolve(domainsData));
   const content = templateString
     .replace(/#TEMPLATE_TIME#/g, NOW_TIME)
     .replace(/'#TEMPLATE_CONTENT#'/g, JSON.stringify(domains, null, 2));
-
-  saveFile(dist, content);
+  writeFileSync(resolve(dist), content, 'utf-8');
   console.log(`[${NOW_TIME}] 🗻 iPac ${dist} Build Success! `);
 }
